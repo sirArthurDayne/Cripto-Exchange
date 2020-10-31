4356b7c3bdae79dafe94272041313a5877fe49b7
sirArthurDayne <xavierivan2000@hotmail.com>
2020-10-31 12:45:17 -0500

    succefully link tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div id="app" class="bg-blue-400">
+        <img alt="Vue logo" src="./assets/logo.png" />
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
 </template>
 
 <script>
